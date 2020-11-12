fef9b0c202971abacec85fd9319065748d8f2d4d
origin <valeriya_stan@mail.ru>
2020-11-13 00:12:55 +0300

    next

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,3 +1,5 @@
+// бургер
+
 menu.onclick = function() {
     var x = document.getElementById("topnav");
 
@@ -7,3 +9,37 @@ menu.onclick = function() {
         x.className = "header-nav";
     }
 }
+
+let btnsFilterContainer = document.querySelector('.portfolio-navigation'); 
+let removeClass = function(arr, className){
+    arr
+    .filter(btn => btn.classList.contains(className))
+    .map(btn => btn.classList.remove(className));
+};
+ 
+// соритровка на главной странице
+
+btnsFilterContainer.addEventListener('click', function(e) {
+    let target = e.target;
+    if (!e.target.closest('button')) return;
+    let btn = e.target.closest('button');
+    if (btn.classList.contains('active')) return;
+    let btns = document.querySelectorAll('.portfolio-navigation button');
+
+let arrBtns = Array.prototype.slice.call(btns);
+
+    removeClass(arrBtns, 'active');
+
+    btn.classList.add('active');
+
+    let btnId = btn.id;
+    let blocks = document.querySelectorAll('.block')
+    let arrBlocks = Array.prototype.slice.call(blocks);
+
+    removeClass(arrBlocks, 'hide');
+    if (btnId == 'all') return;
+
+    arrBlocks
+    .filter(block => (block.dataset.var != btnId))
+    .map(block => block.classList.add('hide'));
+});
