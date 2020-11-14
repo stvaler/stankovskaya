a03ad3b8f1741c642ba3c1f724a574e7387f3675
origin <valeriya_stan@mail.ru>
2020-11-15 02:19:25 +0300

    finish

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,17 +10,14 @@
 //     }
 // }
 
-// let btnsFilterContainer = document.querySelector('.portfolio-navigation'); 
-// let removeClass = function(arr, className){
-//     arr
-//     .filter(btn => btn.classList.contains(className))
-//     .map(btn => btn.classList.remove(className));
-// };
-
-
-
-
 // соритровка на главной странице
+
+let btnsFilterContainer = document.querySelector('.portfolio-navigation'); 
+let removeClass = function(arr, className){
+    arr
+    .filter(btn => btn.classList.contains(className))
+    .map(btn => btn.classList.remove(className));
+};
 
 btnsFilterContainer.addEventListener('click', function(e) {
     let target = e.target;
@@ -48,35 +45,41 @@ let arrBtns = Array.prototype.slice.call(btns);
 });
 
 // слайдер
+// $(document).ready(function(){
+//   $('.your-class').slick({
+//     setting-name: setting-value
+//   });
+// });
 
-$('.slide-team').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    variableWidth: true,
-    responsive: [
-        {
-          breakpoint: 960,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 720,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 540,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
 
-      ]
-});
+// $('.slide-team').slick({
+//     slidesToShow: 4,
+//     slidesToScroll: 1,
+//     variableWidth: true,
+//     responsive: [
+//         {
+//           breakpoint: 960,
+//           settings: {
+//             slidesToShow: 3,
+//             slidesToScroll: 1,
+//             infinite: true,
+//             dots: true
+//           }
+//         },
+//         {
+//           breakpoint: 720,
+//           settings: {
+//             slidesToShow: 2,
+//             slidesToScroll: 1
+//           }
+//         },
+//         {
+//           breakpoint: 540,
+//           settings: {
+//             slidesToShow: 1,
+//             slidesToScroll: 1
+//           }
+//         }
+
+//       ]
+// });
--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ let path = {
         css: source_folder + "/scss/style.scss",
         js: source_folder + "/js/script.js",
         img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
-        fonts: source_folder + "/fonts/*.ttf",
+        fonts: source_folder + '/fonts/*.{ttf,otf,woff,woff2}',
     },
     watch:{
         html: source_folder + "/**/*.html",
